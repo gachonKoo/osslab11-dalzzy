0594ed1bcbc7ba621708b19a280dc62a2a8c5c32
Dahyeon <dahhyeon22@gmail.com>
2025-11-16 22:11:25 +0900

    feat: lab11 제출

--- tester.py
+++ tester.py
@@ -1,7 +1,8 @@
-import math
+from geo.utils import area
 
-c = 5.0
-area = 314.1592653589793
+c = 5.0               
+r = 10                
+calculated_area = area(r)
 
 print(f"c = {c}")
-print(f"area = {area}")
+print(f"area = {calculated_area}")
